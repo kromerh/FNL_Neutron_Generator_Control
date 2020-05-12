7b06978c3d930f5418220bc14a645ab4a84ad67f
Heiko Kromer <heiko.kromer@gmail.com>
2020-05-12 17:00:59 +0200

    pressure

--- 02.Sensors/02.Pressure_twofast-rpi3-3/Python/run_read_pressure.sh
+++ 02.Sensors/02.Pressure_twofast-rpi3-3/Python/run_read_pressure.sh
@@ -1,1 +1,2 @@
-python ~/FNL_Neutron_Generator_Control/02.Sensors/02.Pressure_twofast-rpi3-3/Python/read_pressure.py
+source activate readout
+python3 ~/FNL_Neutron_Generator_Control/02.Sensors/02.Pressure_twofast-rpi3-3/Python/read_pressure.py
