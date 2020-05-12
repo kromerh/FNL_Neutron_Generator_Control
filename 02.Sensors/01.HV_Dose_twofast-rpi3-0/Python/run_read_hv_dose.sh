580537d5ede4cac7689acb453fe3a79d79193e75
Heiko Kromer <heiko.kromer@gmail.com>
2020-05-12 16:47:21 +0200

    hv dose

--- 02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/run_read_hv_dose.sh
+++ 02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/run_read_hv_dose.sh
@@ -1,2 +1,2 @@
-python ~/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/read_hv_dose.py
+python3 ~/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/read_hv_dose.py
 
