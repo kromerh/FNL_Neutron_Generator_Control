e038f44c777c5b26ba099ac74b0246b26cd12d12
Heiko Kromer <heiko.kromer@gmail.com>
2020-09-21 16:20:54 +0200

    cleanup test

--- 01.Database/cleanup_daily.py
+++ 01.Database/cleanup_daily.py
@@ -6,8 +6,6 @@ import pandas as pd
 import datetime
 
 
-
-
 # Connect to the database
 
 # read password and user to database
@@ -25,17 +23,78 @@ sql_engine = sql.create_engine(connect_string)
 
 
 
-print(user, pw, host, db)
+
+# Path to leakage current correction data file
+path_LUT_leakage_current = '/home/pi/FNL_Neutron_Generator_Control/03.Dash/01.V1/calibration/LUT_leakage_current.csv'
+
+# Path to LUT for dose rate to neutron output
+path_LUT_dose_neutron_output = '/home/pi/FNL_Neutron_Generator_Control/03.Dash/01.V1/calibration/LUT_dose_neutron_output.csv'
 
 
 
-# hv_dose
-def read_hv_dose(sql_engine):
+
+def read_table(sql_engine, table):
 	"""
-	Reads all entries from the hv_dose table and returns as dataframe
+	Reads all entries from the table and returns as dataframe
 	"""
-	query = f"SELECT * FROM live_hv_dose;"
+	query = f"SELECT * FROM \"{table}\";"
 	df = pd.read_sql(query, sql_engine)
 	df['date'] = pd.to_datetime(df['date'].values)
 
 	return df
+
+def leakage_current_correction(path_LUT):
+
+	data_I_leak = pd.read_csv(path_LUT, index_col=0)
+	X = data_I_leak['HV_voltage'].values.astype(np.float64)
+	Y = data_I_leak['HV_current'].values.astype(np.float64)
+	interp_leak = interp1d(X, Y, fill_value='extrapolate')
+
+	return interp_leak
+
+def dose_to_output(path_LUT):
+
+	data_mcnp_LUT = pd.read_csv(path_LUT, index_col=0)
+	# position of the sphere is east
+	interp_dose = interp1d(data_mcnp_LUT.index, data_mcnp_LUT['E'].values, fill_value='extrapolate')
+
+	return interp_dose
+
+
+def cleanup_live_hv_dose(sql_engine):
+	"""
+	Cleans up the live_hv_dose and copies it into the storage table.
+	"""
+	connection = sql_engine.raw_connection()
+	cur = connection.cursor()
+
+	try:
+		# load the live_hv_dose
+		df = read_table(sql_engine, table='live_hv_dose')
+
+		# calculate dose_rate
+		df['dose_rate'] = df['dose_voltage'] * 3000 / 5.5
+
+		# leakage current correction
+		interp_leak = leakage_current_correction(path_LUT_leakage_current)
+		df['HV_current'] = df['HV_current'] - interp_leak(df['HV_voltage'].values)
+
+		idx = df[df['HV_current'] < 0].index # set negative current values to 0
+		df.loc[idx, 'HV_current'] = 0
+
+		# compute neutron output from dose
+		interp_dose = dose_to_output(path_LUT_dose_neutron_output)
+		df['neutron_output'] = df['dose'].values * (interp_dose(df_hv_dose['HV_voltage'].values) / 100)
+
+		print(df)
+		# save to the storage
+		# df.to_sql(name=myName, con=connection, if_exists='append', index=False)
+
+		connection.commit()
+	except Exception as e:
+		connection.rollback()
+		modal_body = 'There was an error.'
+		raise e
+
+
+cleanup_live_hv_dose(sql_engine)
