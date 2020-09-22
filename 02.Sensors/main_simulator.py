3bfb85bc628ff7b31e8a84350de704c955b9b06a
Heiko Kromer <heiko.kromer@gmail.com>
2020-09-22 10:15:27 +0200

    simulate all

--- 02.Sensors/main_simulator.py
+++
@@ -1,6 +0,0 @@
-/usr/bin/python3.7 '/home/pi/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/hv_and_dose_simulate_PSI.py' &
-/usr/bin/python3.7 '/home/pi/FNL_Neutron_Generator_Control/02.Sensors/02.Pressure_twofast-rpi3-3/Python/pressure_simulate_PSI.py' &
-/usr/bin/python3.7 '/home/pi/FNL_Neutron_Generator_Control/02.Sensors/03.D2flow_twofast-rpi3-4/Python/d2flow_simulate_PSI.py' &
-/usr/bin/python3.7 '/home/pi/FNL_Neutron_Generator_Control/02.Sensors/04.Microwave_twofast-rpi3-5/Python/mw_simulate_PSI.py' &
-/usr/bin/python3.7 '/home/pi/FNL_Neutron_Generator_Control/02.Sensors/05.Water_leakage_twofast-rpi3-7/Python/leak_simulate_PSI.py' &
-/usr/bin/python3.7 '/home/pi/FNL_Neutron_Generator_Control/02.Sensors/06.Reference_detectors_pi-neutimag-due4/Python/refDet_simulate_PSI.py' &
