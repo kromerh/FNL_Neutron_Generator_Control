e10ea9ed70f1609ef6610dde4dbc0623d34eca98
Heiko Kromer <heiko.kromer@gmail.com>
2020-09-22 10:00:03 +0200

    simulate all

--- 02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/HV_Dose_Simulator.py
+++ 02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/HV_Dose_Simulator.py
@@ -13,30 +13,30 @@ class HV_Dose_Simulator(object):
 		super(HV_Dose_Simulator, self).__init__()
 
 
-	# Settings for the simulator
-	# path
-	PATH_CREDENTIALS = '~/credentials.pw'
-	HV_CALIB_FILE = '~/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/HV_readout_calibration.txt'
-	I_CALIB_FILE = '~/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/I_readout_calibration.txt'
+		# Settings for the simulator
+		# path
+		self.PATH_CREDENTIALS = '~/credentials.pw'
+		self.HV_CALIB_FILE = '~/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/HV_readout_calibration.txt'
+		self.I_CALIB_FILE = '~/FNL_Neutron_Generator_Control/02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/I_readout_calibration.txt'
 
-	# readout frequency
-	FREQUENCY = 1 # sleep time in second
-
-
-	# values to save
-	HV_VOLTAGE_1 = 1.2 # V
-	HV_VOLTAGE_2 = 3.2 # V
-
-	HV_CURRENT_1 = 0.1 # V
-	HV_CURRENT_2 = 0.9 # V
-
-	DOSE_VOLTAGE_1 = 1 # V
-	DOSE_VOLTAGE_2 = 1.9 # V
+		# readout frequency
+		self.FREQUENCY = 1 # sleep time in second
 
 
-	ALTERNATE_FREQ = 30 # how many seconds to switch from one value to the next
+		# values to save
+		self.HV_VOLTAGE_1 = 1.2 # V
+		self.HV_VOLTAGE_2 = 3.2 # V
 
-	VERBOSE = True
+		self.HV_CURRENT_1 = 0.1 # V
+		self.HV_CURRENT_2 = 0.9 # V
+
+		self.DOSE_VOLTAGE_1 = 1 # V
+		self.DOSE_VOLTAGE_2 = 1.9 # V
+
+
+		self.ALTERNATE_FREQ = 30 # how many seconds to switch from one value to the next
+
+		self.VERBOSE = True
 
 
 	# read password and user to database
@@ -77,13 +77,13 @@ class HV_Dose_Simulator(object):
 		# calibrate HV voltage
 
 		# correct the HV that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
-		df_HV_LT = pd.read_csv(HV_CALIB_FILE, delimiter="\t")
+		df_HV_LT = pd.read_csv(self.HV_CALIB_FILE, delimiter="\t")
 		# print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
 		# interpolation function
 		interp_HV_voltage = interp1d(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'], fill_value="extrapolate")
 
 		# correct the current that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
-		df_HV_I_LT = pd.read_csv(I_CALIB_FILE, delimiter="\t")
+		df_HV_I_LT = pd.read_csv(self.I_CALIB_FILE, delimiter="\t")
 		# print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
 		# interpolation function
 		interp_HV_current = interp1d(df_HV_I_LT['Current_read'], df_HV_I_LT['HV_current'])
@@ -92,26 +92,26 @@ class HV_Dose_Simulator(object):
 		# readout of the arduino
 		# pi_flush(arduinoPort)
 		cnt = 0
-		HV_VOLTAGE = HV_VOLTAGE_2
-		HV_CURRENT = HV_CURRENT_2
-		DOSE_VOLTAGE = DOSE_VOLTAGE_2
+		HV_VOLTAGE = self.HV_VOLTAGE_2
+		HV_CURRENT = self.HV_CURRENT_2
+		DOSE_VOLTAGE = self.DOSE_VOLTAGE_2
 		alternate = True
 		while True:
 		    try:
-		        if cnt == ALTERNATE_FREQ:
+		        if cnt == self.ALTERNATE_FREQ:
 		            cnt = 0
 		            if alternate:
-		                HV_VOLTAGE = HV_VOLTAGE_1
-		                HV_CURRENT = HV_CURRENT_1
-		                DOSE_VOLTAGE = DOSE_VOLTAGE_1
+		                HV_VOLTAGE = self.HV_VOLTAGE_1
+		                HV_CURRENT = self.HV_CURRENT_1
+		                DOSE_VOLTAGE = self.DOSE_VOLTAGE_1
 		                alternate = False
 		            else:
-		                HV_VOLTAGE = HV_VOLTAGE_2
-		                HV_CURRENT = HV_CURRENT_2
-		                DOSE_VOLTAGE = DOSE_VOLTAGE_2
+		                HV_VOLTAGE = self.HV_VOLTAGE_2
+		                HV_CURRENT = self.HV_CURRENT_2
+		                DOSE_VOLTAGE = self.DOSE_VOLTAGE_2
 		                alternate = True
 
-		        experiment_id = get_experiment_id(sql_engine, VERBOSE)
+		        experiment_id = get_experiment_id(sql_engine, self.VERBOSE)
 		        dose_voltage = float(DOSE_VOLTAGE)
 		        HV_current = float(HV_CURRENT)  # 0 - 2 mA
 		        HV_voltage = float(HV_VOLTAGE)  # -(0-150) kV
@@ -119,7 +119,7 @@ class HV_Dose_Simulator(object):
 		        HV_current = float(interp_HV_current(HV_CURRENT))
 		        saveDB(experiment_id, dose_voltage, HV_current, HV_voltage, VERBOSE)
 
-		        sleep(FREQUENCY)
+		        sleep(self.FREQUENCY)
 		        cnt += 1
 		    except KeyboardInterrupt:
 		        print('Ctrl + C. Exiting. Flushing serial connection.')
