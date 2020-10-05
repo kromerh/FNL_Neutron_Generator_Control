a5f9e793a1edecadbfaee88f8982b92dc89d99ba
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 13:37:01 +0200

    microwave

--- 02.Sensors/04.Microwave_twofast-rpi3-5/Python/1_turn_MW_ON.py
+++ 02.Sensors/04.Microwave_twofast-rpi3-5/Python/1_turn_MW_ON.py
@@ -187,9 +187,11 @@ def live(mw_ip):
 		# read settings from database
 		experiment_id, mw_fp_set, mw_freq_set = get_experiment_id(sql_engine, verbose=True)
 		mw_fp_set = min(mw_fp_set, 200) # maximum 200
+		mw_fp_set = int(mw_fp_set)
 
 		mw_freq_set = 10 * mw_freq_set # convert to 10 times MHz
 		mw_freq_set = min(mw_freq_set, 25000) # maximum 2.5 GHz
+		mw_freq_set = int(mw_freq_set)
 		print(' ')
 		# sys.stdout.write(f"\n {mw_fp_set}, {mw_freq_set} ")
 #
