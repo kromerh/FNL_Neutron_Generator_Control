ec638fe1ed42599edc882a0a41815ff001fec9fa
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 13:13:41 +0200

    microwave

--- 02.Sensors/04.Microwave_twofast-rpi3-5/Python/1_turn_MW_ON.py
+++ 02.Sensors/04.Microwave_twofast-rpi3-5/Python/1_turn_MW_ON.py
@@ -3,6 +3,10 @@ from time import sleep
 import sys
 import getopt
 from time import sleep
+import serial
+import pandas as pd
+import pymysql
+import re
 
 # MAC Address of the microwave generator
 # MAC Address: 00:80:A3:C2:AB:65 (Lantronix)
@@ -11,13 +15,66 @@ from time import sleep
 # sudo tcpdump -i eth1
 
 
-# for ii in range(0,65535):
-# 	returnlist = []
-# 	for jj in range(0,2000):
-# 		regs_list_1 = ModbusClient.read_holding_registers(ii, jj)
-# 		returnlist.append(regs_list_1)
-# 	if not None in returnlist:
-# 		print(ii, returnlist)
+def live(mw_ip):
+
+mw_ip = str(mw_ip)
+
+
+
+# sudo
+try:
+	# c = ModbusClient(host="169.254.150.42", port=502, auto_open=True, auto_close=True)
+	c = ModbusClient(host=mw_ip, port=502, auto_open=True, auto_close=True)
+	# c = ModbusClient(host="169.254.240.1", port=502, auto_open=True, auto_close=True)
+except ValueError:
+	print("Error with host or port params")
+
+
+# def save_power_to_DB(FP, RP, power_setpoint):
+# 	# Create a Cursor object to execute queries.
+# 	cur = db.cursor()
+# 	try:
+# 		cur.execute("""INSERT INTO microwave_generator_power (FP, RP, power_setpoint) VALUES (%(FP)s, %(RP)s, %(power_setpoint)s)""" % {"FP": FP, "RP": RP, "power_setpoint": power_setpoint})
+# 	except:
+# 		cur.rollback()
+
+# 	db.commit()
+# 	cur.close()
+
+# def save_freq_to_DB(frequency, frequency_setpoint):
+# 	# Create a Cursor object to execute queries.
+# 	cur = db.cursor()
+# 	try:
+# 		cur.execute("""INSERT INTO microwave_generator_frequency (frequency, frequency_setpoint) VALUES (%(frequency)s, %(frequency_setpoint)s)""" % {"frequency": frequency, "frequency_setpoint": frequency_setpoint})
+# 	except:
+# 		cur.rollback()
+
+# 	db.commit()
+# 	cur.close()
+
+# def save_status_to_DB(status):
+# 	print(status)
+# 	# Create a Cursor object to execute queries.
+# 	cur = db.cursor()
+# 	try:
+# 		cur.execute("""INSERT INTO microwave_generator_state (status) VALUES (\"%(status)s\")""" % {"status": status})
+# 	except:
+# 		cur.rollback()
+
+# 	db.commit()
+# 	cur.close()
+
+
+# bool to store if all the settings are set
+RAMP_SET = False
+RAMP_TIME_SET = False
+FP_SET = False
+RP_SET = False
+MODE_SET = False
+MW_ON = False
+FREQ_SET = False
+
+FREQUENCY_SETPOINT = 24400
 
 def send_heartbeat(ModbusClient):
 	# sends MODBUS heart beat
@@ -28,36 +85,36 @@ def set_start_mode_ramp(ModbusClient):
 	# Sets the start mode to ramp
 	# c is ModbusClient
 	wr = ModbusClient.write_single_register(3,2)
-	print('set_start_mode_ramp:' + str(int(wr)))
+	# print('set_start_mode_ramp:' + str(int(wr)))
 	return wr
 
 def set_start_time(ModbusClient):
 	# Sets the start time to 60s
 	# c is ModbusClient
 	wr = ModbusClient.write_single_register(4,60)
-	print('set_start_time:' + str(int(wr)))
+	# print('set_start_time:' + str(int(wr)))
 	return wr
 
 def set_FW_power(ModbusClient):
 	# Sets the forward power set point to 200 W
 	# c is ModbusClient
-	wr = ModbusClient.write_single_register(0,200)
 	# wr = ModbusClient.write_single_register(0,200)
-	print('set_FW_power:' + str(int(wr)))
+	wr = ModbusClient.write_single_register(0,10)
+	# print('set_FW_power:' + str(int(wr)))
 	return wr
 
 def set_RP(ModbusClient):
 	# Sets the reflected power set point to 100 W
 	# c is ModbusClient
 	wr = ModbusClient.write_single_register(1,100)
-	print('set_RP:' + str(int(wr)))
+	# print('set_RP:' + str(int(wr)))
 	return wr
 
 def set_freq(ModbusClient):
-	# Sets the reflected power set point to 100 W
+	# Sets the frequency before the autotuning
 	# c is ModbusClient
-	wr = ModbusClient.write_single_register(9,24400)
-	print('set_freq:' + str(int(wr)))
+	wr = ModbusClient.write_single_register(9,FREQUENCY_SETPOINT)
+	# print('set_freq:' + str(int(wr)))
 	return wr
 
 def set_microwave_mode(ModbusClient):
@@ -66,8 +123,8 @@ def set_microwave_mode(ModbusClient):
 	# c is ModbusClient
 	bit_addr = 2
 	bit_value = 146 # 0 1 0 0 1 0 0 1
-	wr = ModbusClient.write_single_register(bit_addr, bit_value)
-	print('set_microwave_mode:' + str(int(wr)))
+	wr = c.write_single_register(bit_addr, bit_value)
+	# print('set_microwave_mode:' + str(int(wr)))
 	return wr
 
 def set_microwave_ON(ModbusClient):
@@ -76,99 +133,125 @@ def set_microwave_ON(ModbusClient):
 	# c is ModbusClient
 	bit_addr = 2
 	bit_value = 210 # 0 1 0 0 1 0 1 1
-	wr = ModbusClient.write_single_register(bit_addr, bit_value)
-	print('set_microwave_ON:' + str(int(wr)))
+	wr = c.write_single_register(bit_addr, bit_value)
+	# print('set_microwave_ON:' + str(int(wr)))
 	return wr
 
 
 
 def read_fault_present(ModbusClient):
 	# reads if fault present
-	r0 = ModbusClient.read_holding_registers(105, 1)
-	if (len(r0) > 0) and (r0[0] == 128):
-		# something returned and message is Moduel Ready for Microwaves
-		print('Ready for microwaves ')
-		print(r0)
-	else:
-		print('Fault present (105):')
-		print(r0)
-		# read the type of fault
-		r1 = ModbusClient.read_holding_registers(104, 1)
-		print('Type of fault:')
-		print(r1)
+	r0 = c.read_holding_registers(104, 1)
+	r1 = c.read_holding_registers(105, 1)
+
+	return [r0[0], r1[0]]
 
 def read_FP(ModbusClient):
 	# reads forward power
-	r0 = ModbusClient.read_holding_registers(102, 10)
-	print('read_FP :')
-	print(r0)
+	r0 = c.read_holding_registers(102, 1)
+	# print('read_FP :')
+	# print(r0)
+	return r0
 
 def read_RP(ModbusClient):
 	# reads reflected power
-	r0 = ModbusClient.read_holding_registers(103, 10)
-	print('read_RP:')
-	print(r0)
+	r0 = c.read_holding_registers(103, 1)
+	# print('read_RP:')
+	# print(r0[0])
+	return r0
 
 def read_set_FP(ModbusClient):
 	# reads setpoint power
-	r0 = ModbusClient.read_holding_registers(100, 10)
-	print('read_set_FP')
-	print(r0)
+	r0 = c.read_holding_registers(100, 1)
+	# print('read_set_FP')
+	# print(r0)
+	return r0
 
 def read_freq(ModbusClient):
 	# reads current frequency
-	r0 = ModbusClient.read_holding_registers(112, 10)
-	print('read_freq:')
-	print(r0)
+	r0 = c.read_holding_registers(112, 1)
+	# print('read_freq:')
+	# print(r0)
+	return r0
 
-def live(ip_address):
-	while True:
-		c = ModbusClient(host=f"{ip_address}", port=502, auto_open=True, auto_close=True)
 
-		# bool to store if all the settings are set
-		RAMP_SET = False
-		RAMP_TIME_SET = False
-		FP_SET = False
-		RP_SET = False
-		MODE_SET = False
-		MW_ON = False
-		FREQ_SET = False
 
-		# sent hearbeat
-		send_heartbeat(c)
+while True:
+	# sent hearbeat
+	send_heartbeat(c)
 
-		# set start mode to tamp
-		if RAMP_SET == False:
-			RAMP_SET = set_start_mode_ramp(c)
+	# set start mode to tamp
+	if RAMP_SET == False:
+		RAMP_SET = set_start_mode_ramp(c)
 
-		# set start time 60 s
-		if RAMP_TIME_SET == False:
-			RAMP_TIME_SET = set_start_time(c)
+	# set start time 60 s
+	if RAMP_TIME_SET == False:
+		RAMP_TIME_SET = set_start_time(c)
 
-		# set the forward power set point to 200 W
-		if FP_SET == False:
-			FP_SET = set_FW_power(c)
+	# set the forward power set point to 200 W
+	if FP_SET == False:
+		FP_SET = set_FW_power(c)
 
-		# set the reflected power set point to 100 W
-		if RP_SET == False:
-			RP_SET = set_RP(c)
+	# set the reflected power set point to 100 W
+	if RP_SET == False:
+		RP_SET = set_RP(c)
 
-		# set the reflected power set point to 100 W
-		if FREQ_SET == False:
-			FREQ_SET = set_freq(c)
+	# set the reflected power set point to 100 W
+	if FREQ_SET == False:
+		FREQ_SET = set_freq(c)
 
-		# set the microwave mode:
-		if MODE_SET == False:
-			 MODE_SET = set_microwave_mode(c)
+	# set the microwave mode:
+	if MODE_SET == False:
+		 MODE_SET = set_microwave_mode(c)
 
-		read_fault_present(c)
-		read_FP(c)
-		read_RP(c)
-		read_set_FP(c)
-		read_freq(c)
+	status = read_fault_present(c)
+	forward_power = read_FP(c)
+	reflected_power = read_RP(c)
+	setpoint_power = read_set_FP(c)
+	frequency_read = read_freq(c)
 
-		# set the microwaves ON:
-		MW_ON = set_microwave_ON(c)
+
+	# set the microwaves ON:
+	if MW_ON == False:
+		MW_ON = set_microwave_ON(ModbusClient)
+
+	print(status, forward_power, reflected_power, setpoint_power, frequency_read)
+
+	# save to DB
+	# save_power_to_DB(forward_power[0], reflected_power[0], setpoint_power[0])
+	# save_freq_to_DB(frequency_read[0], FREQUENCY_SETPOINT/10)
+
+	status.insert(0, '104:')
+	status.insert(2, ', 105:')
+	status = [str(s) for s in status]
+	# save_status_to_DB(' '.join(status))
+	print(status)
+	sleep(0.1)
+# while True:
+# 	set_microwave_mode(c)
+
+# while True:
+# 	wr = c.write_single_register(20, 128) # modbus heartbeat
+# 	rr = c.read_holding_registers(99, 1)
+# 	r0 = c.read_holding_registers(105, 1)
+# 	print(r0)
+
+# addr = 104
+# regs_list_1 = c.read_holding_registers(addr, 20)
+# print(addr)
+# print(regs_list_1)
+
+# addr = 108
+# regs_list_1 = c.read_holding_registers(addr, 20)
+# print(addr)
+# print(regs_list_1)
+
+
+# addr = 102
+# regs_list_1 = c.read_holding_registers(addr, 20)
+# print(addr)
+# print(regs_list_1)
+
 
 
 
