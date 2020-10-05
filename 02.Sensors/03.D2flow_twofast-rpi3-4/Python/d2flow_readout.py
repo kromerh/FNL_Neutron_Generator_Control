7c49353d35398fda6e180d8cc6eaf4fb9eec2264
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 12:15:09 +0200

    d2flow readout

--- 02.Sensors/03.D2flow_twofast-rpi3-4/Python/d2flow_readout.py
+++ 02.Sensors/03.D2flow_twofast-rpi3-4/Python/d2flow_readout.py
@@ -47,7 +47,7 @@ def get_flow_meter_control_values(sql_engine, verbose=False):
 	query = "SELECT experiment_id, d2flow_set FROM experiment_control"
 	df = pd.read_sql(query, sql_engine)
 
-	setpoint_voltage = df[:,'d2flow_set'].values[0]
+	setpoint_voltage = df['d2flow_set'].values[0]
 	experiment_id = df['experiment_id'].values[0]
 
 
