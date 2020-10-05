78f81ce14119adb2e717c3f1af693c9fdc015b94
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 10:44:47 +0200

    hv and dose readout

--- 02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/hv_and_dose_readout.py
+++ 02.Sensors/01.HV_Dose_twofast-rpi3-0/Python/hv_and_dose_readout.py
@@ -13,7 +13,7 @@ import pandas as pd
 import getopt
 
 
-PATH_CREDENTIALS = r'../../credentials.pw'
+PATH_CREDENTIALS = r'/home/pi/credentials.pw' # on the RPi3
 ARDUINO_PORT = '/dev/ttyACM0'
 VERBOSE = True
 
@@ -41,17 +41,17 @@ def get_experiment_id(sql_engine, verbose=False):
 
     experiment_id = df['experiment_id'].values[0]
 
-    if verbose: print(f"Experiment id is {experiment_id}")
+    if verbose: sys.stdout.write(f"Experiment id is {experiment_id}")
 
     return experiment_id
 
 
-def saveDB(experiment_id, voltage_IS, voltage_VC, verbose=False):
+def saveDB(experiment_id, HV_voltage, HV_current, dose_voltage, verbose=False):
     # Create a Cursor object to execute queries.
-    query = f"""INSERT INTO live_pressure (experiment_id, voltage_IS, voltage_VC) VALUES (\"{experiment_id}\", \"{voltage_IS}\", \"{voltage_VC}\");"""
+    query = f"""INSERT INTO live_hv_dose (experiment_id, HV_voltage, HV_current, dose_voltage) VALUES (\"{experiment_id}\", \"{HV_voltage}\", \"{HV_current}\", \"{dose_voltage}\");"""
     sql_engine.execute(sql.text(query))
 
-    if verbose: print(query)
+    if verbose: sys.stdout.write(query)
 
 def fun_read_serial_ports():
     return list(serial.tools.list_ports.comports())
@@ -61,8 +61,8 @@ def pi_flush(serial_port):
     serialArduino.flushInput()  #flush input buffer, discarding all its contents
     serialArduino.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
 
-def pi_read():
-    serialArduino = serial.Serial(port=arduinoPort, baudrate=9600)
+def pi_read(serial_port):
+    serialArduino = serial.Serial(port=serial_port, baudrate=9600)
     while (serialArduino.inWaiting() == 0):  # wait for incoming data
         pass
     valueRead = serialArduino.readline()
@@ -76,31 +76,55 @@ def pi_read():
         valueRead = '-1'
     return valueRead
 
+# this might be obsolete, not sure
+# # calibrate HV voltage
+
+# # correct the HV that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
+# df_HV_LT = pd.read_csv('/home/pi/Documents/HV_readout_calibration.txt', delimiter="\t")
+# # print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
+# # interpolation function
+# interp_HV_voltage = interp1d(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
+
+# # correct the current that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
+# df_HV_I_LT = pd.read_csv('/home/pi/Documents/I_readout_calibration.txt', delimiter="\t")
+# # print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
+# # interpolation function
+# interp_HV_current = interp1d(df_HV_I_LT['Current_read'], df_HV_I_LT['HV_current'])
+
 def read_live():
     while True:
         try:
-            # read arduino
-            ardRead = pi_read()
-            s = ardRead.rstrip().split()
-            sys.stdout.write('... reading out pressure ...')
-            sys.stdout.write(f'{s}')
-            if len(s) == 5:  # V1 V2 extractionOn
-                print(s)
-                volt_1 = s[0]
-                volt_2 = s[1]
+            experiment_id = get_experiment_id(sql_engine, VERBOSE)
 
-                voltage_IS = float(volt_1)
-                voltage_VC = float(volt_2)
-                print(voltage_IS, voltage_VC)
-                # saveDB(experiment_id, voltage_IS, voltage_VC, VERBOSE)
+            # read arduino
+            ardRead = pi_read(ARDUINO_PORT)
+            s = ardRead.rstrip().split()
+            now = datetime.datetime.now()
+            now = now.strftime(format='%Y-%m-%d %H:%M:%S')
+            print(' ')
+            if len(s) == 5:
+                voltage_dose = float(s[2])
+                HV_current = float(s[3])  # 0 - 2 mA
+                HV_voltage = float(s[4])  # -(0-150) kV
+                # HV_voltage = float(interp_HV_voltage(HV_voltage))
+                # HV_current = float(interp_HV_current(HV_current))
+                # print(HV_voltage)
+
+                voltage_IS = float(s[0])
+                voltage_VC = float(s[1])
+                sys.stdout.write('Reading hv and dose voltages  ...')
+                sys.stdout.write(f'{now}, HV: {HV_voltage}, I: {HV_current}, dose: {dose_voltage}')
+                # saveDB(experiment_id, HV_voltage, HV_current, dose_voltage, VERBOSE)
             sleep(0.1)
 
         except KeyboardInterrupt:
             print('Ctrl + C. Exiting. Flushing serial connection.')
-            pi_flush(arduinoPort)
+            pi_flush(ARDUINO_PORT)
             sys.exit(1)
         finally:
-            pi_flush(arduinoPort)
+            pi_flush(ARDUINO_PORT)
+
+
 
 if __name__ == '__main__':
     # Get the arguments from the command-line except the filename
@@ -122,37 +146,3 @@ if __name__ == '__main__':
         # Print something useful
         print('Error! usage: hv_and_dose_readout.py --MODE. MODE can only be live')
         sys.exit(2)
-
-
-
-
-def pi_flush(serial_port):
-    serialArduino = serial.Serial(port=serial_port, baudrate=9600)
-    serialArduino.flushInput()  #flush input buffer, discarding all its contents
-    serialArduino.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
-
-def pi_read():
-    while (serialArduino.inWaiting() == 0):  # wait for incoming data
-        pass
-    valueRead = serialArduino.readline()
-    try:
-        valueRead = (valueRead.decode('utf-8')).strip()
-    except UnicodeDecodeError:
-        valueRead = '-1'
-    return valueRead
-
-# calibrate HV voltage
-
-# correct the HV that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
-df_HV_LT = pd.read_csv('/home/pi/Documents/HV_readout_calibration.txt', delimiter="\t")
-# print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
-# interpolation function
-interp_HV_voltage = interp1d(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
-
-# correct the current that the arduino reads. This is done using the dose_lookup_table which relates the pi dose with the displayed dose.
-df_HV_I_LT = pd.read_csv('/home/pi/Documents/I_readout_calibration.txt', delimiter="\t")
-# print(df_HV_LT['Voltage_read'], df_HV_LT['HV_voltage'])
-# interpolation function
-interp_HV_current = interp1d(df_HV_I_LT['Current_read'], df_HV_I_LT['HV_current'])
-
-
