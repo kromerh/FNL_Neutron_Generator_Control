08d5569f6d59eb5219d53f56257e3d5808e71408
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 12:09:43 +0200

    d2flow readout

---
+++ 02.Sensors/03.D2flow_twofast-rpi3-4/Python/d2flow_readout.py
@@ -0,0 +1,163 @@
+import serial
+import serial.tools.list_ports
+import re
+import sys
+import pymysql
+from time import sleep
+import pandas as pd
+import numpy as np
+import sqlalchemy as sql
+import datetime
+import time
+import pandas as pd
+import getopt
+
+
+PATH_CREDENTIALS = r'/home/pi/credentials.pw' # on the RPi3
+ARDUINO_PORT = '/dev/ttyACM0'
+VERBOSE = True
+
+
+
+
+# connect to database
+credentials = pd.read_csv(PATH_CREDENTIALS, header=0)
+
+user = credentials['username'].values[0]
+pw = credentials['password'].values[0]
+host = str(credentials['hostname'].values[0])
+db = str(credentials['db'].values[0])
+
+connect_string = 'mysql+pymysql://%(user)s:%(pw)s@%(host)s:3306/%(db)s'% {"user": user, "pw": pw, "host": host, "db": db}
+sql_engine = sql.create_engine(connect_string)
+
+# connect to arduino
+serialArduino = serial.Serial(port=ARDUINO_PORT, baudrate=9600)
+
+
+
+
+def get_flow_meter_control_values(sql_engine, verbose=False):
+
+	query = "SELECT experiment_id, d2flow_set FROM experiment_control"
+	df = pd.read_sql(sql_engine, mysql_connection)
+
+	setpoint_voltage = df.loc[:,'d2flow_set'].values[0]
+    experiment_id = df['experiment_id'].values[0]
+
+
+	if verbose: sys.stdout.write(f"Experiment id: {experiment_id}, Setpoint: {setpoint_voltage} " )
+
+	return experiment_id, setpoint_voltage
+
+def read_live():
+    while True:
+        try:
+            experiment_id, setpoint_voltage = get_flow_meter_control_values(sql_engine, VERBOSE)
+            print(experiment_id, setpoint_voltage)
+            # # read arduino
+            # ardRead = pi_read(ARDUINO_PORT)
+            # s = ardRead.rstrip().split()
+            # now = datetime.datetime.now()
+            # now = now.strftime(format='%Y-%m-%d %H:%M:%S')
+            # print(' ')
+            # if len(s) == 5:
+            #     dose_voltage = float(s[2])
+            #     HV_current = float(s[3])  # 0 - 2 mA
+            #     HV_voltage = float(s[4])  # -(0-150) kV
+            #     # HV_voltage = float(interp_HV_voltage(HV_voltage))
+            #     # HV_current = float(interp_HV_current(HV_current))
+            #     # print(HV_voltage)
+
+            #     voltage_IS = float(s[0])
+            #     voltage_VC = float(s[1])
+            #     sys.stdout.write('Reading hv and dose voltages  ...')
+            #     sys.stdout.write(f'{now}, HV: {HV_voltage}, I: {HV_current}, dose: {dose_voltage} ')
+            #     saveDB(experiment_id, HV_voltage, HV_current, dose_voltage, VERBOSE)
+            sleep(0.1)
+
+        except KeyboardInterrupt:
+            print('Ctrl + C. Exiting. Flushing serial connection.')
+            pi_flush(ARDUINO_PORT)
+            sys.exit(1)
+        finally:
+            pi_flush(ARDUINO_PORT)
+
+
+
+if __name__ == '__main__':
+    # Get the arguments from the command-line except the filename
+    argv = sys.argv[1:]
+
+    try:
+        if len(argv) == 1:
+            MODE = argv[0]
+            if MODE == '--live':
+                read_live()
+            else:
+                print('Error! usage: d2flow_readout.py --MODE. MODE can only be live')
+                sys.exit(2)
+        else:
+            print('Error! usage: d2flow_readout.py --MODE. MODE can only be live')
+            sys.exit(2)
+
+    except getopt.GetoptError:
+        # Print something useful
+        print('Error! usage: d2flow_readout.py --MODE. MODE can only be live')
+        sys.exit(2)
+
+
+
+
+
+
+
+
+
+
+
+# ser = serial.Serial(arduinoPort, 9600)
+# print('Serial connected at ' + str(arduinoPort))
+# sleep(1)
+# # val = 0.5 # Below 32 everything in ASCII is gibberish
+# while True:
+# 	try:
+# 		# SETPOINT VALUE OF FLOW METER
+# 		# read the database for the setpoint value
+# 		setpoint_voltage = getFlowMeterControlValues()
+
+# 		# convert
+# 		valueSend = str(setpoint_voltage)
+# 		# print("Sending value to Arduino " + valueSend)
+# 		# send
+# 		ser.write(valueSend.encode()) # Convert the decimal number to ASCII then send it to the Arduino
+
+# 		print("Successfully sent to Arduino:" + str(valueSend.encode()))
+
+# 		sleep(0.5) # Delay
+
+# 		# READING OF FLOW METER
+# 		valueRead = ser.readline(500) # b'V_1 1.30, 4.20, V_out 215.04\r\n'
+
+# 		print('Raw reading from Arduino :' + str(valueRead)) # Read the newest output from the Arduino
+# 		voltageStr = str(valueRead).split(',')
+
+# 		voltageStr = voltageStr[0]
+
+
+
+# 		t = re.findall(r'V_1 (.+)', voltageStr)
+
+# 		if len(t) > 0:
+# 			voltage = t[0]
+# 			# print(voltage)
+# 			saveFlowMeterVoltageToDB(voltage, setpoint_voltage) # save into DB
+
+# 		sleep(0.5) # Delay
+# 		ser.flushInput()  #flush input buffer, discarding all its contents
+# 		ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+# 	except KeyboardInterrupt:
+# 		print('Ctrl + C. Exiting. Flushing serial connection.')
+# 		ser.flushInput()  #flush input buffer, discarding all its contents
+# 		ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+# 		sys.exit(1)
---
+++ 02.Sensors/04.Microwave_twofast-rpi3-5/Python/1_turn_MW_ON.py
@@ -0,0 +1,188 @@
+from pyModbusTCP.client import ModbusClient
+from time import sleep
+
+# MAC Address of the microwave generator
+# MAC Address: 00:80:A3:C2:AB:65 (Lantronix)
+
+# sudo
+
+
+# for ii in range(0,65535):
+# 	returnlist = []
+# 	for jj in range(0,2000):
+# 		regs_list_1 = c.read_holding_registers(ii, jj)
+# 		returnlist.append(regs_list_1)
+# 	if not None in returnlist:
+# 		print(ii, returnlist)
+
+# bool to store if all the settings are set
+RAMP_SET = False
+RAMP_TIME_SET = False
+FP_SET = False
+RP_SET = False
+MODE_SET = False
+MW_ON = False
+FREQ_SET = False
+
+def send_heartbeat(ModbusClient):
+	# sends MODBUS heart beat
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(20, 128) # modbus heartbeat
+
+def set_start_mode_ramp(ModbusClient):
+	# Sets the start mode to ramp
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(3,2)
+	print('set_start_mode_ramp:' + str(int(wr)))
+	return wr
+
+def set_start_time(ModbusClient):
+	# Sets the start time to 60s
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(4,60)
+	print('set_start_time:' + str(int(wr)))
+	return wr
+
+def set_FW_power(ModbusClient):
+	# Sets the forward power set point to 200 W
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(0,200)
+	print('set_FW_power:' + str(int(wr)))
+	return wr
+
+def set_RP(ModbusClient):
+	# Sets the reflected power set point to 100 W
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(1,100)
+	print('set_RP:' + str(int(wr)))
+	return wr
+
+def set_freq(ModbusClient):
+	# Sets the reflected power set point to 100 W
+	# c is ModbusClient
+	wr = ModbusClient.write_single_register(9,24400)
+	print('set_freq:' + str(int(wr)))
+	return wr
+
+def set_microwave_mode(ModbusClient):
+	# Sets the microwave mode:
+	#	autotuning on, reflected power RP limitation, reset faults
+	# c is ModbusClient
+	bit_addr = 2
+	bit_value = 146 # 0 1 0 0 1 0 0 1
+	wr = c.write_single_register(bit_addr, bit_value)
+	print('set_microwave_mode:' + str(int(wr)))
+	return wr
+
+def set_microwave_ON(ModbusClient):
+	# Sets the microwave mode:
+	#	autotuning on, reflected power RP limitation, MW ON, reset faults
+	# c is ModbusClient
+	bit_addr = 2
+	bit_value = 210 # 0 1 0 0 1 0 1 1
+	wr = c.write_single_register(bit_addr, bit_value)
+	print('set_microwave_ON:' + str(int(wr)))
+	return wr
+
+
+
+def read_fault_present(ModbusClient):
+	# reads if fault present
+	r0 = c.read_holding_registers(105, 1)
+	if (len(r0) > 0) and (r0[0] == 128):
+		# something returned and message is Moduel Ready for Microwaves
+		print('Ready for microwaves ')
+		print(r0)
+	else:
+		print('Fault present:')
+		print(r0)
+		# read the type of fault
+		r1 = c.read_holding_registers(104, 1)
+		print('Type of fault:')
+		print(r1)
+
+def read_FP(ModbusClient):
+	# reads forward power
+	r0 = c.read_holding_registers(102, 10)
+	print('read_FP :')
+	print(r0)
+
+def read_RP(ModbusClient):
+	# reads reflected power
+	r0 = c.read_holding_registers(103, 10)
+	print('read_RP:')
+	print(r0)
+
+def read_set_FP(ModbusClient):
+	# reads setpoint power
+	r0 = c.read_holding_registers(100, 10)
+	print('read_set_FP')
+	print(r0)
+
+def read_freq(ModbusClient):
+	# reads current frequency
+	r0 = c.read_holding_registers(112, 10)
+	print('read_freq:')
+	print(r0)
+
+def live(ip_address):
+	while True:
+		c = ModbusClient(host=f"{ip_address}", port=502, auto_open=True, auto_close=True)
+
+		# sent hearbeat
+		send_heartbeat(c)
+
+		# set start mode to tamp
+		if RAMP_SET == False:
+			RAMP_SET = set_start_mode_ramp(c)
+
+		# set start time 60 s
+		if RAMP_TIME_SET == False:
+			RAMP_TIME_SET = set_start_time(c)
+
+		# set the forward power set point to 200 W
+		if FP_SET == False:
+			FP_SET = set_FW_power(c)
+
+		# set the reflected power set point to 100 W
+		if RP_SET == False:
+			RP_SET = set_RP(c)
+
+		# set the reflected power set point to 100 W
+		if FREQ_SET == False:
+			FREQ_SET = set_freq(c)
+
+		# set the microwave mode:
+		if MODE_SET == False:
+			 MODE_SET = set_microwave_mode(c)
+
+		read_fault_present(c)
+		read_FP(c)
+		read_RP(c)
+		read_set_FP(c)
+		read_freq(c)
+
+		# set the microwaves ON:
+		if MW_ON == False:
+			MW_ON = set_microwave_ON(c)
+
+		sleep(0.01)
+
+if __name__ == '__main__':
+    # Get the arguments from the command-line except the filename
+    argv = sys.argv[1:]
+
+    try:
+        if len(argv) == 1:
+        	ip_address = argv[0]
+        	live(ip_address)
+
+        else:
+            print('Error! usage: .py --ip_address. ip_address must be provided!')
+            sys.exit(2)
+
+    except getopt.GetoptError:
+        # Print something useful
+        print('Error! usage: .py --ip_address. ip_address must be provided!')
+        sys.exit(2)
+
