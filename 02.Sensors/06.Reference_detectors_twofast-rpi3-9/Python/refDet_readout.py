d3fb68e43332c2963f02ef38cec1909af1e0e6d5
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 11:03:14 +0200

    refdet readout

--- 02.Sensors/06.Reference_detectors_twofast-rpi3-9/Python/refDet_readout.py
+++ 02.Sensors/06.Reference_detectors_twofast-rpi3-9/Python/refDet_readout.py
@@ -17,7 +17,7 @@ PATH_CREDENTIALS = r'/home/pi/credentials.pw' # on the RPi3
 ARDUINO_PORT = '/dev/ttyACM0'
 VERBOSE = True
 
-
+print(1)
 # connect to database
 credentials = pd.read_csv(PATH_CREDENTIALS, header=0)
 
