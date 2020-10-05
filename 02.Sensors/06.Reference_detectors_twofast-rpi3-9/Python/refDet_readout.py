5fe446e6127643c9cb619d556729044f491288dc
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-05 11:02:22 +0200

    hv and dose readout

--- 02.Sensors/06.Reference_detectors_twofast-rpi3-9/Python/refDet_readout.py
+++ 02.Sensors/06.Reference_detectors_twofast-rpi3-9/Python/refDet_readout.py
@@ -85,6 +85,7 @@ def read_live():
             s = ardRead.rstrip().split()
             now = datetime.datetime.now()
             now = now.strftime(format='%Y-%m-%d %H:%M:%S')
+            print(1)
             print(' ')
             if len(s) == 7:
                 ard_time = s[1]
@@ -92,6 +93,7 @@ def read_live():
                 counts_D2 = s[4]
                 counts_D3 = s[5]
                 counts_D4 = s[6]
+                sys.stdout.write(f'{now}, D1: {counts_D1}, D2: {counts_D2}, D3: {counts_D3}, D4: {counts_D4} ')
                 if float(ard_time) >= 30000.0:
                     sys.stdout.write('Reading reference detectors  ...')
                     sys.stdout.write(f'{now}, D1: {counts_D1}, D2: {counts_D2}, D3: {counts_D3}, D4: {counts_D4} ')
