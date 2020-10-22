811f93929bb6ebdd03ac3525c170765c48698d00
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-22 14:51:42 +0200

    operation report

--- 03.Dash/01.V1/layout_operation_report.py
+++ 03.Dash/01.V1/layout_operation_report.py
@@ -66,11 +66,11 @@ layout_operation_report = html.Div(id='operation_report_parent',children=
                                                         'backgroundColor': 'white',
                                                         'fontWeight': 'bold'
                                                     },
-                                                )
+                                                ),
                                             ],
-                                            className="table__1",
+                                            className="table__1 four columns",
                                             ),
-                        ],
+                                ],
                         ),
                     dcc.Tab(
                         label="STORAGE",
