2841f235527da48136162370f05e439e162a3fb3
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-22 13:57:52 +0200

    operation report

--- 03.Dash/01.V1/layout_operation_report.py
+++ 03.Dash/01.V1/layout_operation_report.py
@@ -8,7 +8,49 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 
-layout_operation_report = html.Div(id='sensor_readout_parent',children=
-    [],
+layout_operation_report = html.Div(id='operation_report_parent',children=
+    [
+        html.Div(
+            [
+                html.Div(
+                    [
+
+                    ],
+                className="one-third column"
+                ),
+                html.Div(
+                    [
+                        html.H4("Operation report", style={"text-align":"center", "color":'gray'}),
+                        html.Br()
+                    ],
+                className="one-third column"
+                ),
+                html.Div(
+                    [
+
+                    ],
+                className="one-third column"
+                ),
+
+            ],
+            className="row flex-display"
+        ),
+        dcc.Tabs(
+                id="tabs",
+                value="tab_live",
+                children=[
+                    dcc.Tab(
+                        label="LIVE",
+                        value="tab_live",
+                        children=[],
+                        ),
+                    dcc.Tab(
+                        label="STORAGE",
+                        value="tab_storage",
+                        children=[],
+                        ),
+                    ],
+                ),
+    ],
     style={"display": "flex", "flex-direction": "column"},
 )
