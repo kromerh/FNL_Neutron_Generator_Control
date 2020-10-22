2aa5a58918ad2cdce467ebe8fa7e3ea95d53f5ad
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-22 15:02:33 +0200

    operation report

--- 03.Dash/01.V1/layout_operation_report.py
+++ 03.Dash/01.V1/layout_operation_report.py
@@ -83,6 +83,46 @@ layout_operation_report = html.Div(id='operation_report_parent',children=
                                             className="three columns",
                                             style = {"margin-left": "5%"},
                                         ),
+                                    html.Div(
+                                            [
+                                                dcc.Graph(
+                                                        id="graph_HV",
+                                                        figure={
+                                                            "data": [
+                                                                {"x": [1, 2, 3], "y": [4, 1, 2], "type": "scatter"},
+                                                                {"x": [1, 2, 3], "y": [2, 4, 5], "type": "scatter"},
+                                                            ],
+                                                            "layout": {
+                                                                "height": 200,  # px
+                                                                "margin": dict(t=10, b=15, l=50, r=50)
+                                                            },
+                                                        },
+                                                    ),
+
+                                            ],
+                                            className="pretty_container five columns",
+                                            style={"margin-left": "2%"}
+                                        ),
+                                    html.Div(
+                                            [
+                                                dcc.Graph(
+                                                        id="graph_dose",
+                                                        figure={
+                                                            "data": [
+                                                                {"x": [1, 2, 3], "y": [4, 1, 2], "type": "scatter"},
+                                                                {"x": [1, 2, 3], "y": [2, 4, 5], "type": "scatter"},
+                                                            ],
+                                                            "layout": {
+                                                                "height": 200,  # px
+                                                                "margin": dict(t=10, b=15, l=50, r=50)
+                                                            },
+                                                        },
+                                                    ),
+
+                                            ],
+                                            className="pretty_container five columns",
+                                            style={"margin-left": "2%"}
+                                        ),
 
                                 ],
                         ),
