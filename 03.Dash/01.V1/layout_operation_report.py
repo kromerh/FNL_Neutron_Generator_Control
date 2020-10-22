bf6f31fe450c5669bbb4aa51ca360a28b7c08fe5
Heiko Kromer <heiko.kromer@gmail.com>
2020-10-22 14:38:52 +0200

    operation report

--- 03.Dash/01.V1/layout_operation_report.py
+++ 03.Dash/01.V1/layout_operation_report.py
@@ -2,6 +2,8 @@ import pymysql
 import sqlalchemy as sql
 
 import dash
+import dash_table
+
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
