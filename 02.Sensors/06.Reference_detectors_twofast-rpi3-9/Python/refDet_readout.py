93b5c13ecbf2b1d4a6c94aa212a8303834c81225
Heiko Kromer <31862508+kromerh@users.noreply.github.com>
2020-11-17 10:39:16 +0100

    Update refDet_readout.py

--- 02.Sensors/06.Reference_detectors_twofast-rpi3-9/Python/refDet_readout.py
+++ 02.Sensors/06.Reference_detectors_twofast-rpi3-9/Python/refDet_readout.py
@@ -29,112 +29,111 @@ connect_string = 'mysql+pymysql://%(user)s:%(pw)s@%(host)s:3306/%(db)s'% {"user"
 sql_engine = sql.create_engine(connect_string)
 
 def serial_close(ser):
-    ser.close()
+	ser.close()
 
 def serial_open(ARDUINO_PORT):
-    serialArduino = serial.Serial(port=ARDUINO_PORT, baudrate=9600)
+	serialArduino = serial.Serial(port=ARDUINO_PORT, baudrate=9600)
 # connect to arduino
 
 
 
 def get_experiment_id(sql_engine, verbose=False):
-    query = f"SELECT experiment_id FROM experiment_control;"
-    df = pd.read_sql(query, sql_engine)
+	query = f"SELECT experiment_id FROM experiment_control;"
+	df = pd.read_sql(query, sql_engine)
 
-    experiment_id = df['experiment_id'].values[0]
+	experiment_id = df['experiment_id'].values[0]
 
-    if verbose: sys.stdout.write(f"Experiment id is {experiment_id}")
+	if verbose: sys.stdout.write(f"Experiment id is {experiment_id}")
 
-    return experiment_id
+	return experiment_id
 
 
 def saveDB(experiment_id, ard_time, counts_D1, counts_D2, counts_D3, counts_D4, verbose=False):
-    # Create a Cursor object to execute queries.
-    query = f"""INSERT INTO live_ref_det (experiment_id, ard_time, counts_D1, counts_D2, counts_D3, counts_D4) VALUES (\"{experiment_id}\", \"{ard_time}\", \"{counts_D1}\", \"{counts_D2}\", \"{counts_D3}\", \"{counts_D4}\");"""
-    sql_engine.execute(sql.text(query))
+	# Create a Cursor object to execute queries.
+	query = f"""INSERT INTO live_ref_det (experiment_id, ard_time, counts_D1, counts_D2, counts_D3, counts_D4) VALUES (\"{experiment_id}\", \"{ard_time}\", \"{counts_D1}\", \"{counts_D2}\", \"{counts_D3}\", \"{counts_D4}\");"""
+	sql_engine.execute(sql.text(query))
 
-    if verbose: sys.stdout.write(query)
+	if verbose: sys.stdout.write(query)
 
 def fun_read_serial_ports():
-    return list(serial.tools.list_ports.comports())
+	return list(serial.tools.list_ports.comports())
 
 def pi_flush(serial_port):
-    serialArduino = serial.Serial(port=serial_port, baudrate=9600)
-    serialArduino.flushInput()  #flush input buffer, discarding all its contents
-    serialArduino.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+	serialArduino = serial.Serial(port=serial_port, baudrate=9600)
+	serialArduino.flushInput()  #flush input buffer, discarding all its contents
+	serialArduino.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
 
-def pi_read(serial_port):
-    serialArduino = serial.Serial(port=serial_port, baudrate=9600)
-    while (serialArduino.inWaiting() == 0):  # wait for incoming data
-        pass
-    valueRead = serialArduino.readline(500)
-    # print(valueRead)
-    try:
-        valueRead = (valueRead.decode('utf-8')).strip()
-        serialArduino.flushInput()  #flush input buffer, discarding all its contents
-        serialArduino.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
-       # print(valueRead)
-    except UnicodeDecodeError:
-        valueRead = '-1'
-    return valueRead
+def pi_read(serialArduino):
+	while (serialArduino.inWaiting() == 0):  # wait for incoming data
+		pass
+	valueRead = serialArduino.readline(500)
+	# print(valueRead)
+	try:
+		valueRead = (valueRead.decode('utf-8')).strip()
+		serialArduino.flushInput()  #flush input buffer, discarding all its contents
+		serialArduino.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+	   # print(valueRead)
+	except UnicodeDecodeError:
+		valueRead = '-1'
+	return valueRead
 
-ser = serial_open(ARDUINO_PORT)
 
 def read_live():
-    while True:
-        try:
-            # experiment_id = get_experiment_id(sql_engine, VERBOSE)
-            
-            # read arduino
-            ardRead = pi_read(ARDUINO_PORT)
-            s = ardRead.rstrip().split()
-            now = datetime.datetime.now()
-            now = now.strftime(format='%Y-%m-%d %H:%M:%S')
-            print(f"{now}")
-           
-            if len(s) == 7:
-                ard_time = s[1]
-                counts_D1 = s[3]
-                counts_D2 = s[4]
-                counts_D3 = s[5]
-                counts_D4 = s[6]
-                print(ard_time, s)
-                if float(ard_time) >= 30000.0:
-                    sys.stdout.write('Reading reference detectors  ...')
-                    sys.stdout.write(f'{now}, D1: {counts_D1}, D2: {counts_D2}, D3: {counts_D3}, D4: {counts_D4} ')
-                    ser.close()
-                    pi_flush(ARDUINO_PORT)
-                    serialArduino = serial_open(arduinoPort)
-                    # saveDB(experiment_id, ard_time, counts_D1, counts_D2, counts_D3, counts_D4, VERBOSE)
+	while True:
+		try:
+			# experiment_id = get_experiment_id(sql_engine, VERBOSE)
+			ser = serial_open(ARDUINO_PORT)
+			
+			# read arduino
+			ardRead = pi_read(ARDUINO_PORT)
+			s = ardRead.rstrip().split()
+			now = datetime.datetime.now()
+			now = now.strftime(format='%Y-%m-%d %H:%M:%S')
+			print(f"{now}")
+		   
+			if len(s) == 7:
+				ard_time = s[1]
+				counts_D1 = s[3]
+				counts_D2 = s[4]
+				counts_D3 = s[5]
+				counts_D4 = s[6]
+				print(ard_time, s)
+				if float(ard_time) >= 30000.0:
+					sys.stdout.write('Reading reference detectors  ...')
+					sys.stdout.write(f'{now}, D1: {counts_D1}, D2: {counts_D2}, D3: {counts_D3}, D4: {counts_D4} ')
+					ser.close()
+					pi_flush(ARDUINO_PORT)
+					serialArduino = serial_open(arduinoPort)
+					# saveDB(experiment_id, ard_time, counts_D1, counts_D2, counts_D3, counts_D4, VERBOSE)
 
-            sleep(0.1)
+			sleep(0.1)
 
-        except KeyboardInterrupt:
-            print('Ctrl + C. Exiting. Flushing serial connection.')
-            pi_flush(ARDUINO_PORT)
-            sys.exit(1)
-        finally:
-            pi_flush(ARDUINO_PORT)
+		except KeyboardInterrupt:
+			print('Ctrl + C. Exiting. Flushing serial connection.')
+			pi_flush(ARDUINO_PORT)
+			sys.exit(1)
+		finally:
+			pi_flush(ARDUINO_PORT)
 
 
 
 if __name__ == '__main__':
-    # Get the arguments from the command-line except the filename
-    argv = sys.argv[1:]
+	# Get the arguments from the command-line except the filename
+	argv = sys.argv[1:]
 
-    try:
-        if len(argv) == 1:
-            MODE = argv[0]
-            if MODE == '--live':
-                read_live()
-            else:
-                print('Error! usage: refDet_readout.py --MODE. MODE can only be live')
-                sys.exit(2)
-        else:
-            print('Error! usage: refDet_readout.py --MODE. MODE can only be live')
-            sys.exit(2)
+	try:
+		if len(argv) == 1:
+			MODE = argv[0]
+			if MODE == '--live':
+				read_live()
+			else:
+				print('Error! usage: refDet_readout.py --MODE. MODE can only be live')
+				sys.exit(2)
+		else:
+			print('Error! usage: refDet_readout.py --MODE. MODE can only be live')
+			sys.exit(2)
 
-    except getopt.GetoptError:
-        # Print something useful
-        print('Error! usage: refDet_readout.py --MODE. MODE can only be live')
-        sys.exit(2)
+	except getopt.GetoptError:
+		# Print something useful
+		print('Error! usage: refDet_readout.py --MODE. MODE can only be live')
+		sys.exit(2)
